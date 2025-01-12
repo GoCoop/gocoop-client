21d066cdc7e7015fb2cea758a9a39d21c6261b64
Carlos Lotus <contato@carloslotuspro.com>
2025-01-12 19:05:37 -0300

    UPD focus outline color

--- src/app/components/material/InputField/InputField.tsx
+++ src/app/components/material/InputField/InputField.tsx
@@ -2,7 +2,7 @@ export default function InputField() {
     return (
         <input
             type="text"
-            className="w-full p-4 border-2 rounded-2xl"
+            className="w-full p-4 border-2 rounded-2xl focus:outline-[#5DC04F]"
             placeholder="Busque por uma cooperativa..."
         />
     )
