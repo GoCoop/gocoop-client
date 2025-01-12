275ffa7b80bb8da8976f9d7f325f56519ea6f08b
Carlos Lotus <contato@carloslotuspro.com>
2025-01-12 20:02:16 -0300

    ADD search icon input

--- src/app/components/material/InputField/InputField.tsx
+++ src/app/components/material/InputField/InputField.tsx
@@ -1,9 +1,18 @@
-export default function InputField() {
+import { JSX } from "react";
+
+type Props = {
+    icon?: any;
+}
+
+export default function InputField(props: Props): JSX.Element {
     return (
-        <input
-            type="text"
-            className="w-full p-4 border-2 rounded-2xl focus:outline-[#5DC04F]"
-            placeholder="Busque por uma cooperativa..."
-        />
+        <div className="relative">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-3">{props.icon}</span>
+            <input
+                type="text"
+                className={`w-full p-4 ${[props.icon ? 'pl-10' : 'p-4']} border-2 rounded-2xl focus:outline-[#5DC04F]`}
+                placeholder="Busque por uma cooperativa..."
+            />
+        </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="p-6 h-[calc(100vh-75px)] grid grid-rows-[max-content_max-content_max-content_1fr] gap-4 sm:justify-center">
+      <main className="p-6 h-[calc(100vh-75px)] grid grid-rows-[max-content_max-content_max-content_1fr] gap-4 sm:justify-center sm:pt-20">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="50"
@@ -24,7 +24,34 @@ export default function Home() {
           />
         </svg>
         <h1 className="text-xl">Descubra cooperativas de diferentes ramos em todo o Brasil.</h1>
-        <InputField />
+        <InputField
+          icon={
+            <svg
+              width="24px"
+              height="24px"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#000000"
+            >
+              <path
+                d="M17 17L21 21"
+                stroke="#000000"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+              <path
+                d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
+                stroke="#000000"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+          }
+        />
 
         <div className="grid grid-cols-2 gap-4 place-content-end sm:place-content-start sm:grid-cols-3">
           <Category
