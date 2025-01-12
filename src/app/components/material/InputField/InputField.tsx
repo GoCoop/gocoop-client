9e8936dab3a311d3c287b8dbc701463f15a0b0fc
Carlos Lotus <contato@carloslotuspro.com>
2025-01-12 20:26:21 -0300

    ADD placeholder & icon props

--- src/app/components/material/InputField/InputField.tsx
+++ src/app/components/material/InputField/InputField.tsx
@@ -1,19 +1,24 @@
-import { JSX } from "react";
+import React, { JSX } from "react";
 
 type Props = {
-    icon?: any;
+    placeholder?: string;
+    icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
     autoFocus?: boolean;
 }
 
-export default function InputField(props: Props): JSX.Element {
+export default function InputField({
+    placeholder = 'Digite o texto',
+    icon: Icon,
+    autoFocus = false
+}: Props): JSX.Element {
     return (
         <div className="relative">
-            <span className="absolute inset-y-0 left-0 flex items-center pl-3">{props.icon}</span>
+            {Icon && <span className="absolute inset-y-0 left-0 flex items-center pl-3"> <Icon /> </span>}
             <input
                 type="text"
-                className={`w-full p-4 ${[props.icon ? 'pl-10' : 'p-4']} border-2 rounded-2xl focus:outline-[#5DC04F]`}
-                autoFocus={props.autoFocus}
-                placeholder="Busque por uma cooperativa..."
+                className={`w-full p-4 ${[Icon ? 'pl-10' : 'p-4']} border-2 rounded-2xl focus:outline-[#5DC04F]`}
+                autoFocus={autoFocus}
+                placeholder={placeholder}
             />
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import Header from "./components/core/Header";
 import InputField from "./components/material/InputField/InputField";
 import Category from "./components/material/Category/Category";
 
+// Icons
+import SearchIcon from "./icons/SearchIcon";
+
 export default function Home() {
   return (
     <>
@@ -25,33 +28,9 @@ export default function Home() {
         </svg>
         <h1 className="text-xl">Descubra cooperativas de diferentes ramos em todo o Brasil.</h1>
         <InputField
+          placeholder="Busque por uma cooperativa..."
           autoFocus={true}
-          icon={
-            <svg
-              width="24px"
-              height="24px"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              color="#000000"
-            >
-              <path
-                d="M17 17L21 21"
-                stroke="#000000"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-              <path
-                d="M3 11C3 15.4183 6.58172 19 11 19C13.213 19 15.2161 18.1015 16.6644 16.6493C18.1077 15.2022 19 13.2053 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11Z"
-                stroke="#000000"
-                strokeWidth="1.5"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>
-            </svg>
-          }
+          icon={SearchIcon}
         />
 
         <div className="grid grid-cols-2 gap-4 place-content-end sm:place-content-start sm:grid-cols-3">
