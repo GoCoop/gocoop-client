0af39247a0081aebb6a154b6b64eda19b49eed3f
Carlos Lotus <contato@carloslotuspro.com>
2025-01-11 21:22:22 -0300

    UPD responsive navbar

--- src/app/components/core/Header.tsx
+++ src/app/components/core/Header.tsx
@@ -1,8 +1,14 @@
-import { JSX } from "react";
+import { JSX, useState } from "react";
+import Link from "next/link";
 
 export default function Header(): JSX.Element {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenuBar = () => { setIsMenuOpen(!isMenuOpen) };
+
     return (
-        <header className="w-full p-6 flex justify-between border-b-2">
+        <header className="w-full p-6 flex items-center justify-between border-b">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="25"
@@ -15,38 +21,83 @@ export default function Header(): JSX.Element {
                 />
             </svg>
 
-            <svg
-                width="25px"
-                height="25px"
-                strokeWidth="1.5"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                color="#000000"
-            >
-                <path
-                    d="M3 5H21"
-                    stroke="#000000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                ></path>
-                <path
-                    d="M3 12H21"
-                    stroke="#000000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                ></path>
-                <path
-                    d="M3 19H21"
-                    stroke="#000000"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                ></path>
-            </svg>
+            {
+                isMenuOpen ?
+                    <svg
+                        width="24px"
+                        height="24px"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        color="#000000"
+                        className="sm:hidden"
+                        onClick={() => toggleMenuBar()}
+                    >
+                        <path
+                            d="M6.75827 17.2426L12.0009 12M17.2435 6.75736L12.0009 12M12.0009 12L6.75827 6.75736M12.0009 12L17.2435 17.2426"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>
+                    </svg>
+                    :
+                    <svg
+                        width="25px"
+                        height="25px"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        color="#000000"
+                        className="sm:hidden"
+                        onClick={() => toggleMenuBar()}
+                    >
+                        <path
+                            d="M3 5H21"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>
+                        <path
+                            d="M3 12H21"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>
+                        <path
+                            d="M3 19H21"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        ></path>
+                    </svg>
+            }
 
+            <nav className="hidden sm:block">
+                <ul className="flex gap-6">
+                    <li className="cursor-pointer">Home</li>
+                    <li className="cursor-pointer">Sobre Nós</li>
+                </ul>
+            </nav>
+
+            {
+                isMenuOpen &&
+                <nav className="sm:hidden absolute h-[calc(100vh-75px)] w-screen right-0 bottom-0 left-0 flex justify-center items-center">
+                    <ul className="flex flex-col items-center gap-6">
+                        <li className="cursor-pointer">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className="cursor-pointer">
+                            <Link href="/aboutUs">Sobre Nós</Link>
+                        </li>
+                    </ul>
+                </nav>
+            }
         </header>
     )
 }
