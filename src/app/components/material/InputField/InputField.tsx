e96a600e5df0abfd7db9a979b2117ca884c11f8d
Carlos Lotus <contato@carloslotuspro.com>
2025-01-11 23:49:55 -0300

    ADD material input field component

---
+++ src/app/components/material/InputField/InputField.tsx
@@ -0,0 +1,9 @@
+export default function InputField() {
+    return (
+        <input
+            type="text"
+            className="w-full p-4 border-2 rounded-2xl"
+            placeholder="Busque por uma cooperativa..."
+        />
+    )
+}
