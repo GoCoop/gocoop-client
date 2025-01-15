20493d073230d06efb5a1927670a0150b693ec6f
Carlos Lotus <contato@carloslotuspro.com>
2025-01-14 23:14:04 -0300

    UPD style

--- src/components/core/ResultBox.tsx
+++ src/components/core/ResultBox.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function ResultBox(props: Props) {
     return (
-        <div className="p-4 border-x border-y rounded-lg grid grid-cols-[max-content_1fr] gap-x-4">
+        <div className="p-6 border-x border-y rounded-lg grid grid-cols-[max-content_1fr] gap-x-4 cursor-pointer hover:bg-slate-100">
             <Image
                 className="row-start-1 row-end-3 rounded-full"
                 src={props.imageUrl}
