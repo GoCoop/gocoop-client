1268e0763e39178b1f6ea61f5da4e45a8cbdd0d7
Carlos Lotus <contato@carloslotuspro.com>
2025-01-14 22:15:06 -0300

    ADD props to ResultBox

--- src/app/components/core/ResultBox.tsx
+++ src/app/components/core/ResultBox.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 
-export default function ResultBox() {
+type Props = {
+    name: string;
+    desc: string;
+    imageUrl: string;
+}
+
+export default function ResultBox(props: Props) {
     return (
         <div className="p-4 border-x border-y rounded-lg grid grid-cols-[max-content_1fr] gap-x-4">
             <Image
                 className="row-start-1 row-end-3 rounded-full"
-                src="/agraria-logo.jpg"
+                src={props.imageUrl}
                 alt="Logo da cooperativa"
                 width={50}
                 height={50}
             />
-            <h1 className="text-lg">Agrária</h1>
-            <p className="text-sm">Cooperativa responsável pela produção de malte.</p>
+            <h1 className="text-lg">{props.name}</h1>
+            <p className="text-sm">{props.desc}</p>
         </div>
 
     )
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,6 +7,40 @@ import InputField from "../components/material/InputField/InputField";
 import SearchIcon from "../icons/SearchIcon";
 
 export default function SearchPage() {
+
+    const data = [
+        {
+            id: 1,
+            name: 'Agrária',
+            desc: 'Cooperativa responsável pela produção de malte.',
+            imageUrl: '/agraria-logo.jpg'
+        },
+        {
+            id: 2,
+            name: 'Agrária',
+            desc: 'Cooperativa responsável pela produção de malte.',
+            imageUrl: '/agraria-logo.jpg'
+        },
+        {
+            id: 3,
+            name: 'Agrária',
+            desc: 'Cooperativa responsável pela produção de malte.',
+            imageUrl: '/agraria-logo.jpg'
+        },
+        {
+            id: 4,
+            name: 'Agrária',
+            desc: 'Cooperativa responsável pela produção de malte.',
+            imageUrl: '/agraria-logo.jpg'
+        },
+        {
+            id: 5,
+            name: 'Agrária',
+            desc: 'Cooperativa responsável pela produção de malte.',
+            imageUrl: '/agraria-logo.jpg'
+        }
+    ]
+
     return (
         <>
             <Header />
@@ -20,11 +54,14 @@ export default function SearchPage() {
                 />
 
                 <div className="grid gap-5">
-                    <ResultBox />
-                    <ResultBox />
-                    <ResultBox />
-                    <ResultBox />
-                    <ResultBox />
+                    {data.map(d => (
+                        <ResultBox
+                            key={d.id}
+                            name={d.name}
+                            desc={d.desc}
+                            imageUrl={d.imageUrl}
+                        />
+                    ))}
                 </div>
             </main>
         </>
