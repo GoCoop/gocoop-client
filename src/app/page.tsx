82dce6104daa567beeb391eaea1290537dbb5ca7
Carlos Lotus <contato@carloslotuspro.com>
2025-01-19 16:27:32 -0300

    UPD display three categories in desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       <Modal isOpen={isOpen} onClose={closeModal}>
         <div className="grid gap-6">
           <h2 className="text-xl">Selecione uma categoria</h2>
-          <div className="grid grid-cols-2 gap-4">
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
             {categoriesData && categoriesData.map(cat => (
               <Category
                 key={cat.id}
