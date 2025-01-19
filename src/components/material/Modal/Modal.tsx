3d208c1a8482fe6066da6269c42546f5cbe2afff
Carlos Lotus <contato@carloslotuspro.com>
2025-01-19 16:26:51 -0300

    UPD responsive modal

--- src/components/material/Modal/Modal.tsx
+++ src/components/material/Modal/Modal.tsx
@@ -12,9 +12,9 @@ export default function Modal({ isOpen, onClose, children }: Props): JSX.Element
 
     return (
         <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
-            <div className="relative bg-white p-6 w-full h-full overflow-y-auto sm:max-w-md sm:max-h-[28rem] sm:rounded-lg">
+            <div className="relative bg-white p-6 w-full h-full overflow-y-auto sm:max-w-[33rem] sm:h-auto sm:max-h-[18rem] sm:rounded-lg md:max-h-[24rem]">
                 <button
-                    className="bg-slate-300 p-1 w-6 h-6 rounded-full text-gray-400 flex justify-center items-center hover:text-gray-600"
+                    className="bg-slate-300 p-1 w-6 h-6 rounded-full text-gray-400 flex justify-center items-center place-self-end hover:text-gray-600"
                     onClick={onClose}
                 >&times;</button>
                 {children}
