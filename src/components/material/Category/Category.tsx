103625a56edf28e7af68c8074901281bb110afe3
Carlos Lotus <contato@carloslotuspro.com>
2025-01-19 15:10:16 -0300

    ADD icon component & className props

--- src/components/material/Category/Category.tsx
+++ src/components/material/Category/Category.tsx
@@ -1,14 +1,16 @@
+import Icon from "@/icons/Icon/Icon";
 import { JSX } from "react";
 
 type Props = {
     name: string;
     icon: any;
+    className?: string;
 }
 
 export default function Category(props: Props): JSX.Element {
     return (
-        <button className="w-full p-4 border-x border-y rounded-2xl flex flex-col gap-6 hover:bg-slate-100">
-            {props.icon}
+        <button className={`w-full p-4 border-x border-y rounded-2xl flex flex-col gap-6 hover:bg-slate-100 ${props.className}`}>
+            <Icon icon={props.icon}/>
             <span>{props.name}</span>
         </button>
     )
