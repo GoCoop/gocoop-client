5572148e70907866546429de370f61d35684a6c5
Carlos Lotus <contato@carloslotuspro.com>
2025-02-09 18:09:18 -0300

    ADD type json struct

--- src/dictionaries.ts
+++ src/dictionaries.ts
@@ -2,7 +2,7 @@ import 'server-only'
 
 export type Locales = "pt" | "en";
  
-const dictionaries: any = {
+const dictionaries: Record<Locales, () => Promise<Record<string, any>>> = {
   en: () => import('@/dictionaries/en.json').then((module) => module.default),
   pt: () => import('@/dictionaries/pt.json').then((module) => module.default)
 }
