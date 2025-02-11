f88fdf97e9c9246cad76e514ea47996d3780db20
Carlos Lotus <contato@carloslotuspro.com>
2025-02-10 23:45:54 -0300

    FIX close modal btn to the right in firefox

--- src/components/material/Modal/Modal.tsx
+++ src/components/material/Modal/Modal.tsx
@@ -37,7 +37,7 @@ export default function Modal({
 
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="relative bg-white p-6 w-full h-full overflow-y-auto scrollbar-thin sm:max-w-[33rem] sm:h-auto sm:max-h-[18rem] sm:rounded-lg md:max-h-[24rem]">
+      <div className="relative flex flex-col bg-white p-6 w-full h-full overflow-y-auto scrollbar-thin sm:max-w-[33rem] sm:h-auto sm:max-h-[18rem] sm:rounded-lg md:max-h-[24rem]">
         <button
           className="bg-slate-300 p-1 w-6 h-6 rounded-full text-gray-400 flex justify-center items-center place-self-end hover:text-gray-600"
           onClick={closeModal}
