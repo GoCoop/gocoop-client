0fab36b8c4647af7a1c6fa9082d30cece91d5c23
Carlos Lotus <contato@carloslotuspro.com>
2025-03-12 22:20:13 -0300

    ADD next production config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: (process.env.NEXT_PUBLIC_API_PROTOCOL as "http" | "https") || 'http',
+        hostname: process.env.NEXT_PUBLIC_API_HOSTNAME || 'localhost',
+        pathname: process.env.NEXT_PUBLIC_API_PATHNAME
+        // Add port if localhost
+      }
+    ]
+  }
 };
 
 export default nextConfig;
